27fb41f61f9fc28a85339c1e7d92408639e64505
Lasse Hyldig Hansen <54820693+Lassehhansen@users.noreply.github.com>
2023-02-21 13:43:12 +0100

    Update

--- RandomAgentNoise_Function.R
+++ RandomAgentNoise_Function.R
@@ -1,5 +1,5 @@
-RandomAgentNoise_Function <- function(rate, noise) {
-  choice <-rbinom(1, 1, rate)
+RandomAgentNoise_Function <- function(bias, noise) {
+  choice <-rbinom(1, 1, bias)
   if (rbinom(1, 1, noise) == 1) 
   {choice = rbinom(1, 1, 0.5)}
   return(choice)}
