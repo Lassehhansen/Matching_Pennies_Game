261d28b277bbc58b6267271349b17f470b98902f
Lasse Hyldig Hansen <54820693+Lassehhansen@users.noreply.github.com>
2023-03-02 10:03:50 +0100

    Adding script for simulating multiple memory sizes

---
+++ Matching_Pennies_Game_MultiMemSize.R
@@ -0,0 +1,94 @@
+Matching_Pennies_Game_MultiMemSize = function(n_agents, n_trials, function_give, noise, msize_range, bias, power){
+  
+  # Sourcing functions
+  
+  source("RandomAgentNoise_Function.R")
+  source("WSLSAgent_Function.R")
+  source("MemAgent_Function.R")
+  
+  
+  # Making a counter 
+  
+  counter = 0
+  
+  # Making payoff df 
+  
+  payoff_df <- data.frame()
+  
+  
+  for (a in 1:n_agents){
+    
+    counter = counter + 1
+    
+    for (m in msize_range){
+      
+      # Creating give and take choices
+
+      agent_give_choices <- vector(mode = "numeric", length = n_trials)
+      agent_take_choices <- vector(mode = "numeric", length = n_trials)
+      
+      # Creating give & take payoffs
+      
+      agent_give_payoff_m <- vector(mode = "numeric", length = n_trials)
+      agent_take_payoff_m <- vector(mode = "numeric", length = n_trials)
+      
+      # Creating trial and agent vectors
+      
+      trial <- vector(mode = "numeric", length = n_trials)
+      agent <- vector(mode = "numeric", length = n_trials)
+      
+      for (t in 1:n_trials) {
+        
+        # Get the agent 1 choice
+        if (function_give == "RandomAgentNoise_Function"){
+          agent_give_choice <- RandomAgentNoise_Function(bias, noise)
+        }
+        else if (function_give == "WSLSAgent_Function" & t == 1){
+          agent_give_choice = rbinom(1, 1, bias)
+        }
+        else if (function_give == "WSLSAgent_Function" & t > 1){
+          agent_give_choice <- WSLSAgent_Function(agent_give_choice, agent_give_payoff)
+        }
+        
+        # If this is the first round or the short-term memory agent has not yet accumulated enough memory, choose randomly
+        if (t <= m) {
+          agent_take_choice <- rbinom(1, 1, bias)
+        }
+        else {
+          # Get the agent 2 choice based on its memory
+          agent_take_choice <- MemAgent_Function(agent_give_choices[(t-m):(t-1)], m, bias, power)
+        }
+        
+        # Determine the payoff for each agent
+        if (agent_give_choice == agent_take_choice) {
+          agent_give_payoff <- 1
+          agent_take_payoff <- -1
+        } 
+        else {
+          agent_give_payoff <- -1
+          agent_take_payoff <- 1
+        }
+        
+        # Save the agents' choices for this round
+        agent_give_choices[t] <- agent_give_choice
+        agent_take_choices[t] <- agent_take_choice
+        
+        agent_give_payoff_m[t] <- agent_give_payoff
+        agent_take_payoff_m[t] <- agent_take_payoff
+        
+        trial[t] <-  t
+        agent[t] <- counter
+        
+        payoff_temp = as.data.frame(cbind(agent_give_choices, agent_take_choices, agent_give_payoff_m, agent_take_payoff_m, trial, agent, m))
+        
+      }
+      
+      payoff_df <- rbind(payoff_df, payoff_temp)
+      
+    }
+    
+  }
+  
+  colnames(payoff_df)[ncol(payoff_df)] <- "msize"
+  return(payoff_df)
+}
